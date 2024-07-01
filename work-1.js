e76dcdf2c372c0b92f4e0767c3b96495b49c74f1
jojayeon <jojayeon6152@gmail.com>
2024-07-01 11:15:19 +0900

    타스 연습

---
+++ work-1.js
@@ -0,0 +1,3 @@
+// type name = "앤디"
+var a2 = "조자연";
+console.log(a2);
---
+++ work-1.ts
@@ -0,0 +1,11 @@
+// type name = "앤디"
+
+// let a : name = "조자연"
+
+// console.log(a)
+
+type name2 = string
+
+let a2 : name2 = "조자연"
+
+console.log(a2)
